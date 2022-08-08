e2651bf4ec32472afcfa41e94f27e24d4ed10553
Agnes <agnes.rosa@hotmail.com>
2022-08-08 15:19:13 -0300

    feat: Ad list/show interface

--- src/interfaces/Ad/ad.interface.ts
+++ src/interfaces/Ad/ad.interface.ts
@@ -1,4 +1,4 @@
 import Ad from "../../models/Ad";
 
 export type IAdCreation = Omit<Ad, "id">;
-export type IAdList = Omit<Ad, "id" | "createdOn">;
+export type IAdShow = Omit<Ad, "id" | "createdOn">;
