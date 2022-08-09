c0ce0c3dcb19a128669160d9e3bef70c8455f98c
Victor <vlcampos921@gmail.com>
2022-08-09 10:37:19 -0300

    feat: Create List One Ad Service

---
+++ src/services/Ads/ad.listOne.service.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../../data-source";
+import AppError from "../../errors/AppError";
+import Ad from "../../models/Ad";
+
+const listAdDetails = async (id: string) => {
+  const adsRepository = AppDataSource.getRepository(Ad);
+
+  const ad = await adsRepository.findOneBy({ id: id });
+
+  if (!ad) {
+    throw new AppError("Ad not found or doesn't exists");
+  }
+
+  return ad;
+};
+
+export default listAdDetails;
