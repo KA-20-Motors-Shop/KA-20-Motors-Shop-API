e09abcf721e9c065801727334156bce62ffa514f
Paulo <paulo.r.guarnieri@hotmail.com>
2022-08-11 03:50:26 -0300

    feat: search ad by user feature

--- src/controllers/ads.controller.ts
+++ src/controllers/ads.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import listAdService from "../services/Ads/ad.list.service";
 import createAdService from "../services/Ads/ad.create.service";
+import { adsRoutes } from "../routes/ads.routes";
+import { request } from "http";
+import adListByUserService from "../services/Ads/ad.listbyuser.service";
 
 export default class AdsController {
   static store = async (request: Request, response: Response) => {
@@ -15,7 +18,16 @@ export default class AdsController {
     return response.json(ads);
   };
 
-  static index = async (request: Request, response: Response) => {};
+  static index = async(request: Request, response: Response) => {
+
+  }
+
+  static listAdByUser = async (request: Request, response: Response) => {
+    const {user_id} =  request.params;
+    const ads = await adListByUserService({user_id});
+    
+    return response.json(ads);
+  };
 
   static update = async (request: Request, response: Response) => {};
 
