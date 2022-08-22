508bce27a6615945296891809aa9836adaab868c
Agnes <agnes.rosa@hotmail.com>
2022-08-22 18:07:10 -0300

    feat: comment create controller

--- src/services/Comments/comment.create.service.ts
+++ src/services/Comments/comment.create.service.ts
@@ -2,13 +2,17 @@ import { ICommentCreation } from "../../interfaces/Comment/comment.interface";
 import { AppDataSource } from "../../data-source";
 import Comment from "../../models/Comment";
 
-const createCommentService = async (bodyContent: ICommentCreation) => {
+const createCommentService = async ({
+  description,
+  ad,
+  user,
+}: ICommentCreation) => {
   const commentRepo = AppDataSource.getRepository(Comment);
 
   const newComment = new Comment();
-  newComment.description = bodyContent.description;
-  newComment.ad = bodyContent.ad;
-  newComment.user = bodyContent.user;
+  newComment.description = description;
+  newComment.ad = ad;
+  newComment.user = user;
 
   commentRepo.create(newComment);
   await commentRepo.save(newComment);
